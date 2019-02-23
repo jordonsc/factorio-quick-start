db1ebfba5d4678a7b6e2167f5393fe49d8135e3d
Jordon Scott <jordon@novatek.com.au>
2019-02-23 18:49:46 +1100

    Making some changes in prep for v0.17

--- README.md
+++ README.md
@@ -7,13 +7,10 @@ manually constructing a base with burner inserters.
 
 You gain the annoying/tedious things that take time to rebuild, while keeping the essence of the game in-tact.
 
-Added with the release of Factorio 0.16, a kit that drops a plethora of chests full of enough gear to quickly rebuild a
-mid-game base without the need of a starter base.
 
-Kits
-----
-### Alpha Kit
-By default, on player creation (game start) you get:
+Included
+--------
+On player creation (game start) you get:
 
 * Research completed:
   * All bot speed
@@ -30,43 +27,15 @@ By default, on player creation (game start) you get:
   * A bunch of fast belts & inserters
   * Factories, furnaces and enough kit to set up a power station
   * A handful of base resources (iron, copper, gears, green chips)
-  * A car
+  * A couple cars loaded with more of the above
 
 You should be able to set up a small base and get on your feet, but no additional research is granted.
-
-### Bravo Kit
-Lazy? In addition to the above:
-
-* Research completed:
-  * Optics
-  * Circuit networks
-  * Logistics 2
-  * Automation 2
-* Armour:
-  * 2x additional exoskeletons
-* Items:
-  * Advanced circuits (red chips)
-  * Oil-based factories & refineries
-  * Laboratories
-
-### Charlie Kit
-Got half way through the game and then a new version was released? This kit will take the Bravo kit but also drop a lot
-of chests (900 chests) next to your starting location full of enough gear to build a mid-game base.
-
-Also adds night vision and personal laser defense units to your armour.
-
-NB: The _Charlie Kit_ makes no regard for terrain, your starting chests might be floating on water or over trees.
-
-
-### Omega Kit
-Hardcore? stick to basics. This kit has just a few basics to skip the first few hours only. No roboport. No fast bots.
-
 
 Installing
 ----------
 Build the mod by running `./build.sh`, then copy the zip file created in `build/` to your mods directory in 
-`~/.factorio/mods` (Linux). On Linux, for your current user, you can run `./build.sh && ./install.sh` to quickly build
-and copy the mod.
+`~/.factorio/mods` (Linux) or `AppData\Roaming\Factorio\mods` (Windows). On Linux, for your current user, you can 
+run `./build.sh && ./install.sh` to quickly build and copy the mod.
 
 
 Credit
--- mod/control.lua
+++ mod/control.lua
@@ -10,16 +10,13 @@ script.on_event(defines.events.on_player_created, function(event)
 		defines.inventory.player_vehicle,
 		defines.inventory.player_player_trash
 	}
-	
-	-- Setup kit presets
-	local kits = {}
 
-    -- Alpha kit: quickstart + annoying stuff
-    kits["alpha"] = {}
+	-- Kit settings - change the loadout here
+	local kit = {}
 
-	kits["alpha"]["armourType"] = "power-armor-mk2"
+	kit["armourType"] = "power-armor-mk2"
 
-	kits["alpha"]["quickbar"] = {
+	kit["quickbar"] = {
 		{1, "fast-transport-belt"},
 		{2, "fast-underground-belt"},
 		{3, "fast-splitter"},
@@ -45,23 +42,27 @@ script.on_event(defines.events.on_player_created, function(event)
 		{20, "repair-pack"},
 	}
 
-	kits["alpha"]["items"] = {
-		{kits["alpha"]["armourType"], 1},
+	kit["items"] = {
+		{ kit["armourType"], 1},
+        {"submachine-gun", 1},
+        {"piercing-rounds-magazine", 100},
+
 		{"steel-axe", 10},
 		{"iron-plate", 492},
-		{"copper-plate", 400},
-		{"iron-gear-wheel", 200},
-		{"electronic-circuit", 400},
-		{"fast-transport-belt", 2600},
+        {"iron-gear-wheel", 200},
+        {"copper-plate", 400},
+        {"copper-cable", 400},
+        {"electronic-circuit", 400},
+		{"fast-transport-belt", 2400},
 		{"fast-underground-belt", 150},
 		{"fast-splitter", 150},
-		{"electric-furnace", 100},
-		{"assembling-machine-2", 100},
+		{"electric-furnace", 300},
+		{"assembling-machine-2", 300},
 		{"fast-inserter", 300},
 		{"long-handed-inserter", 150},
 		{"filter-inserter", 50},
 		{"stack-filter-inserter", 50},
-		{"steel-chest", 200},
+		{"steel-chest", 150},
 		{"electric-mining-drill", 150},
 		{"medium-electric-pole", 150},
 		{"big-electric-pole", 100},
@@ -75,246 +76,50 @@ script.on_event(defines.events.on_player_created, function(event)
         {"small-lamp", 100},
         {"deconstruction-planner", 1},
         {"repair-pack", 100},
-        {"car", 1},
-    }
-
-	kits["alpha"]["armourItems"] = {
-		{"fusion-reactor-equipment"},
-		{"fusion-reactor-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"exoskeleton-equipment"},
-		{"exoskeleton-equipment"},
-    }
-
-    kits["alpha"]["technologies"] = {
-        {"toolbelt"},
-        {"character-logistic-slots-1"},
-        {"character-logistic-slots-2"},
-        {"character-logistic-slots-3"},
-        {"character-logistic-slots-4"},
-        {"character-logistic-slots-5"},
-        {"character-logistic-slots-6"},
-        {"auto-character-logistic-trash-slots"},
-		{"worker-robots-speed-1"},
-		{"worker-robots-speed-2"},
-		{"worker-robots-speed-3"},
-		{"worker-robots-speed-4"},
-		{"worker-robots-speed-5"},
-		{"worker-robots-speed-6"},
-		{"worker-robots-storage-1"},
-		{"worker-robots-storage-2"},
-		{"worker-robots-storage-3"},
-    }
-
-    -- Bravo kit: you lazy bastard.
-    kits["bravo"] = {}
-
-	kits["bravo"]["armourType"] = "power-armor-mk2"
-
-	kits["bravo"]["quickbar"] = {
-		{1, "fast-transport-belt"},
-		{2, "fast-underground-belt"},
-		{3, "fast-splitter"},
-		{4, "fast-inserter"},
-		{5, "long-handed-inserter"},
-
-		{6, "stone-wall"},
-		{7, "gate"},
-		{8, "small-lamp"},
-		{9, "steel-chest"},
-		{10, "deconstruction-planner"},
-
-		{11, "rail"},
-        {12, "rail-signal"},
-        {13, "rail-chain-signal"},
-		{14, "filter-inserter"},
-		{15, "stack-filter-inserter"},
-
-		{16, "medium-electric-pole"},
-		{17, "big-electric-pole"},
-		{18, "substation"},
-		{19, "car"},
-		{20, "repair-pack"},
-	}
-
-	kits["bravo"]["items"] = {
-		{kits["bravo"]["armourType"], 1},
-		{"steel-axe", 10},
-		{"iron-plate", 492},
-		{"copper-plate", 400},
-		{"iron-gear-wheel", 200},
-		{"electronic-circuit", 400},
-		{"fast-transport-belt", 2600},
-		{"fast-underground-belt", 150},
-		{"fast-splitter", 150},
-		{"electric-furnace", 100},
-		{"assembling-machine-2", 100},
-		{"fast-inserter", 300},
-		{"long-handed-inserter", 150},
-		{"filter-inserter", 50},
-		{"stack-filter-inserter", 50},
-		{"steel-chest", 200},
-		{"electric-mining-drill", 150},
-		{"medium-electric-pole", 150},
-		{"big-electric-pole", 100},
-		{"substation", 100},
-		{"boiler", 20},
-		{"steam-engine", 40},
-		{"offshore-pump", 10},
-		{"pipe-to-ground", 200},
-		{"pipe", 200},
-		{"construction-robot", 150},
-        {"small-lamp", 100},
-        {"deconstruction-planner", 1},
-        {"repair-pack", 100},
-        {"car", 1},
-
-        {"advanced-circuit", 200},
-        {"chemical-plant", 20},
-        {"oil-refinery", 10},
-        {"pumpjack", 60},
-        {"lab", 10},
-    }
-
-	kits["bravo"]["armourItems"] = {
-		{"fusion-reactor-equipment"},
-		{"fusion-reactor-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"exoskeleton-equipment"},
-		{"exoskeleton-equipment"},
-		{"exoskeleton-equipment"},
-		{"exoskeleton-equipment"},
-    }
-
-    kits["bravo"]["technologies"] = {
-        {"toolbelt"},
-        {"character-logistic-slots-1"},
-        {"character-logistic-slots-2"},
-        {"character-logistic-slots-3"},
-        {"character-logistic-slots-4"},
-        {"character-logistic-slots-5"},
-        {"character-logistic-slots-6"},
-        {"auto-character-logistic-trash-slots"},
-		{"worker-robots-speed-1"},
-		{"worker-robots-speed-2"},
-		{"worker-robots-speed-3"},
-		{"worker-robots-speed-4"},
-		{"worker-robots-speed-5"},
-		{"worker-robots-speed-6"},
-		{"worker-robots-storage-1"},
-		{"worker-robots-storage-2"},
-		{"worker-robots-storage-3"},
-
-        {"circuit-network"},
-        {"logistics"},
-        {"logistics-2"},
-        {"automation"},
-        {"automation-2"},
-        {"optics"},
-    }
-
-
-    -- Charlie kit: designed for quickly rebuilding after upgrading Factorio versions
-    kits["charlie"] = {}
-
-	kits["charlie"]["armourType"] = "power-armor-mk2"
-
-	kits["charlie"]["quickbar"] = {
-		{1, "fast-transport-belt"},
-		{2, "fast-underground-belt"},
-		{3, "fast-splitter"},
-		{4, "fast-inserter"},
-		{5, "long-handed-inserter"},
-
-		{6, "stone-wall"},
-		{7, "gate"},
-		{8, "small-lamp"},
-		{9, "steel-chest"},
-		{10, "deconstruction-planner"},
-
-		{11, "rail"},
-        {12, "rail-signal"},
-        {13, "rail-chain-signal"},
-		{14, "filter-inserter"},
-		{15, "stack-filter-inserter"},
-
-		{16, "medium-electric-pole"},
-		{17, "big-electric-pole"},
-		{18, "substation"},
-		{19, "car"},
-		{20, "repair-pack"},
-	}
-
-	kits["charlie"]["items"] = {
-		{kits["charlie"]["armourType"], 1},
-		{"steel-axe", 10},
-		{"iron-plate", 492},
-		{"copper-plate", 400},
-		{"iron-gear-wheel", 200},
-		{"electronic-circuit", 400},
-		{"fast-transport-belt", 2600},
-		{"fast-underground-belt", 150},
-		{"fast-splitter", 150},
-		{"electric-furnace", 100},
-		{"assembling-machine-2", 100},
-		{"fast-inserter", 300},
-		{"long-handed-inserter", 150},
-		{"filter-inserter", 50},
-		{"stack-filter-inserter", 50},
-		{"steel-chest", 200},
-		{"electric-mining-drill", 150},
-		{"medium-electric-pole", 150},
-		{"big-electric-pole", 100},
-		{"substation", 100},
-		{"boiler", 20},
-		{"steam-engine", 40},
-		{"offshore-pump", 10},
-		{"pipe-to-ground", 200},
-		{"pipe", 200},
-		{"construction-robot", 150},
-        {"small-lamp", 100},
-        {"deconstruction-planner", 1},
-        {"repair-pack", 100},
-        {"car", 1},
-
-        {"advanced-circuit", 200},
-        {"chemical-plant", 20},
-        {"oil-refinery", 10},
-        {"pumpjack", 60},
-        {"lab", 10},
-
+        {"rail", 100},
         {"stone-wall", 100},
         {"gate", 50},
         {"rail-signal", 50},
         {"rail-chain-signal", 50},
     }
 
-	kits["charlie"]["armourItems"] = {
-		{"fusion-reactor-equipment"},
-		{"fusion-reactor-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"personal-roboport-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"battery-mk2-equipment"},
-		{"exoskeleton-equipment"},
-		{"exoskeleton-equipment"},
-		{"exoskeleton-equipment"},
-		{"exoskeleton-equipment"},
-		{"night-vision-equipment"},
-		{"personal-laser-defense-equipment"},
-		{"personal-laser-defense-equipment"},
-		{"personal-laser-defense-equipment"},
-		{"personal-laser-defense-equipment"},
-		{"personal-laser-defense-equipment"},
+    kit["cars"] = {
+        {
+            {"iron-plate", 2000},
+            {"iron-gear-wheel", 1000},
+            {"copper-plate", 2000},
+            {"copper-cable", 2000},
+            {"electronic-circuit", 2000},
+            {"rail", 1000},
+        },
+        {
+            {"fast-transport-belt", 3000},
+            {"fast-underground-belt", 250},
+            {"fast-splitter", 250},
+            {"fast-inserter", 250},
+            {"long-handed-inserter", 250},
+            {"filter-inserter", 250},
+            {"steel-chest", 250},
+            {"small-lamp", 100},
+            {"medium-electric-pole", 300},
+            {"big-electric-pole", 300},
+            {"substation", 150},
+            {"stone-wall", 300},
+        },
     }
 
-    kits["charlie"]["technologies"] = {
+	kit["armourItems"] = {
+		{"fusion-reactor-equipment"},
+		{"fusion-reactor-equipment"},
+		{"personal-roboport-mk2-equipment"},
+		{"personal-roboport-mk2-equipment"},
+		{"battery-mk2-equipment"},
+		{"battery-mk2-equipment"},
+		{"exoskeleton-equipment"},
+		{"exoskeleton-equipment"},
+    }
+
+    kit["technologies"] = {
         {"toolbelt"},
         {"character-logistic-slots-1"},
         {"character-logistic-slots-2"},
@@ -332,56 +137,10 @@ script.on_event(defines.events.on_player_created, function(event)
 		{"worker-robots-storage-1"},
 		{"worker-robots-storage-2"},
 		{"worker-robots-storage-3"},
-
-        {"circuit-network"},
-        {"logistics"},
-        {"logistics-2"},
-        {"automation"},
-        {"automation-2"},
-        {"optics"},
-    }
-
-
-    -- Omega kit: just the super basics
-    kits["omega"] = {}
-
-	kits["omega"]["quickbar"] = {
-		{1, "transport-belt"},
-		{2, "underground-belt"},
-		{3, "splitter"},
-		{4, "inserter"},
-		{10, "deconstruction-planner"},
-	}
-
-	kits["omega"]["items"] = {
-		{"steel-axe", 10},
-		{"iron-plate", 192},
-		{"copper-plate", 100},
-		{"iron-gear-wheel", 100},
-		{"transport-belt", 1500},
-		{"underground-belt", 150},
-		{"splitter", 150},
-		{"assembling-machine-1", 100},
-		{"inserter", 300},
-		{"steel-chest", 50},
-		{"electric-mining-drill", 50},
-		{"medium-electric-pole", 100},
-		{"boiler", 20},
-		{"steam-engine", 40},
-		{"offshore-pump", 10},
-		{"pipe-to-ground", 100},
-		{"pipe", 100},
-        {"deconstruction-planner", 1},
     }
 
     -- end kits
-	
-	local kitSetting = settings.startup["quick-start-boost-kit"].value
-	local kit = kits[kitSetting]
-	if kit == nil then
-		kit = kits["alpha"]
-	end
-	
+
 	-- Unlock technologies (must come before quickbar)
 	if kit["technologies"] ~= nil then
 		for k,v in pairs(kit["technologies"]) do
@@ -394,7 +153,7 @@ script.on_event(defines.events.on_player_created, function(event)
 	if quickbar ~= nil and not quickbar.can_set_filter(1, "transport-belt") then
 		quickbar = player.get_inventory(defines.inventory.god_quickbar)
 	end
-	
+
 	-- Setup quickbar favorites
 	if quickbar ~= nil and quickbar.can_set_filter(1, "transport-belt") ~= nil then
 		quickbar.clear()
@@ -402,7 +161,7 @@ script.on_event(defines.events.on_player_created, function(event)
 			quickbar.set_filter(v[1], v[2])
 		end
 	end
-	
+
 	-- Add items
 	for k,v in pairs(kit["items"]) do
 		player.insert{name = v[1], count = v[2]}
@@ -428,143 +187,21 @@ script.on_event(defines.events.on_player_created, function(event)
 		end
 	end
 
-    -- Place some chests
-    if kitSetting == "charlie" then
-        local pos = player.position
-        local max = 10000
+    -- Place some cars full of goodies
+    local pos = player.position
+    pos.y = pos.y + 2
+    pos.x = pos.x - 2
 
-        function createRow(item, rows, cols)
-            for y=1,rows do
-                for x=1,cols do
-                    pos.x = pos.x + 1
-                    local chest = player.surface.create_entity{name="logistic-chest-passive-provider", position=pos, force=player.force}
-                    chest.insert{name = item, count = max}
-                end
+    for i,c in pairs(kit["cars"]) do
+        local car = player.surface.create_entity{name="car", position=pos, force=player.force}
+        car.insert{name = "solid-fuel", count = 50}
+        car.insert{name = "piercing-rounds-magazine", count = 100}
 
-                if cols == 10 then
-                    pos.x = pos.x - 10
-                    pos.y = pos.y + 1
-                end
-            end
+        pos.x = pos.x + 4
+
+        for k,v in pairs(c) do
+            car.insert{name = v[1], count = v[2]}
         end
-
-        function createFullRow(item, rows)
-            createRow(item, rows, 10)
-        end
-
-        -- Drop some roboports to stop the annoying notification
-        local robopos = player.position
-        robopos.x = robopos.x - 2
-        robopos.y = robopos.y + 16
-        player.surface.create_entity{name="roboport", position=robopos, force=player.force}
-        robopos.x = robopos.x + 38
-        player.surface.create_entity{name="roboport", position=robopos, force=player.force}
-
-
-        -- COLUMN 1
-
-        -- Red belts
-        createFullRow("fast-transport-belt", 10)
-        pos.y = pos.y + 1
-
-        -- Inserters
-        createFullRow("fast-inserter", 5)
-        createFullRow("stack-inserter", 3)
-        createFullRow("long-handed-inserter", 1)
-        createRow("stack-filter-inserter", 1, 5)
-        createRow("filter-inserter", 1, 5)
-        pos.x = pos.x - 10
-        pos.y = pos.y + 2
-
-        -- Factories
-        createFullRow("assembling-machine-2", 1)
-        createFullRow("electric-mining-drill", 1)
-        createFullRow("electric-furnace", 1)
-        createFullRow("solar-panel", 1)
-        createFullRow("accumulator", 1)
-        createFullRow("fast-splitter", 1)
-        createFullRow("fast-underground-belt", 1)
-        createFullRow("concrete", 1)
-        createFullRow("hazard-concrete", 1)
-
-        createRow("oil-refinery", 1, 1)
-        createRow("pumpjack", 1, 2)
-        createRow("chemical-plant", 1, 1)
-        createRow("boiler", 1, 1)
-        createRow("steam-engine", 1, 1)
-        createRow("stone-brick", 1, 1)
-        createRow("radar", 1, 1)
-        createRow("storage-tank", 1, 1)
-        createRow("engine-unit", 1, 1)
-        pos.x = pos.x - 10
-        pos.y = pos.y + 1
-
-
-        -- COLUMN 2
-        pos.x = pos.x + 11
-        pos.y = pos.y - 32
-
-        -- Rail
-        createFullRow("rail", 7)
-        createFullRow("rail-signal", 2)
-        createFullRow("rail-chain-signal", 1)
-        pos.y = pos.y + 1
-
-        -- Power Poles, Misc
-        createFullRow("steel-chest", 3)
-        createFullRow("small-lamp", 1)
-        createFullRow("medium-electric-pole", 2)
-        createFullRow("big-electric-pole", 2)
-        createFullRow("substation", 2)
-        pos.y = pos.y + 1
-
-        -- Walls
-        createFullRow("stone-wall", 8)
-        createFullRow("gate", 2)
-
-
-        -- COLUMN 3
-        pos.x = pos.x + 11
-        pos.y = pos.y - 32
-
-        -- Defence
-        createFullRow("laser-turret", 4)
-        createFullRow("flamethrower-turret", 4)
-        createFullRow("pipe", 1)
-        createFullRow("pipe-to-ground", 1)
-        pos.y = pos.y + 1
-
-        -- Trains, robots
-        createFullRow("locomotive", 1)
-        createFullRow("cargo-wagon", 4)
-        createFullRow("fluid-wagon", 1)
-        createFullRow("train-stop", 1)
-        createFullRow("roboport", 1)
-        createFullRow("construction-robot", 1)
-        createFullRow("logistic-robot", 1)
-        pos.y = pos.y + 1
-
-        -- Resources
-        createFullRow("electronic-circuit", 2)
-        createFullRow("advanced-circuit", 1)
-        createFullRow("speed-module", 1)
-        createFullRow("iron-plate", 1)
-        createFullRow("iron-gear-wheel", 1)
-        createFullRow("copper-plate", 1)
-        createFullRow("copper-cable", 1)
-        createFullRow("landfill", 1)
-        createRow("battery", 1, 1)
-        createRow("plastic-bar", 1, 1)
-        createRow("constant-combinator", 1, 1)
-        createRow("decider-combinator", 1, 1)
-        createRow("arithmetic-combinator", 1, 1)
-        createRow("steam-turbine", 1, 1)
-        createRow("heat-exchanger", 1, 1)
-        createRow("heat-pipe", 1, 1)
-        createRow("logistic-chest-storage", 1, 1)
-        createRow("logistic-chest-passive-provider", 1, 1)
-
-
     end
-	
+
 end)
